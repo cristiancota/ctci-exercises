0356b749a7ea764bd141eefb9ae22f82c5df022c
ccota <crcotag@gmail.com>
2020-05-12 20:17:44 -0700

    zigzag improvement

--- src/leetCode/ZigZag.java
+++ src/leetCode/ZigZag.java
@@ -5,32 +5,19 @@ import java.util.ArrayList;
 public class ZigZag {
     public String convert(String s, int numRows) {
         if (numRows == 1) return s;
-        ArrayList<ArrayList<String>> rows = new ArrayList<>(numRows);
 
-        for (int i = 0; i < numRows; i++) {
-            rows.add(new ArrayList<>());
-        }
+        ArrayList<ArrayList<String>> rows = new ArrayList<>(numRows);
+        for (int i = 0; i < numRows; i++) rows.add(new ArrayList<>()); // initialize rows
 
         int index = 0;
         int i = -1;
         boolean down = true;
+
         while (index < s.length()) {
-            if (i == 0) {
-                down = true;
-            }
-
-            if (i == numRows - 1) {
-                down = false;
-            }
-
-            if (down) {
-                i++;
-            } else {
-                i--;
-            }
-
-            ArrayList<String> strings = rows.get(i);
-            strings.add(String.valueOf(s.charAt(index)));
+            if (i == 0) down = true;
+            if (i == numRows - 1) down = false;
+            i = down ? i + 1 : i - 1;
+            rows.get(i).add(String.valueOf(s.charAt(index)));
             index++;
         }
 
@@ -38,6 +25,5 @@ public class ZigZag {
         rows.forEach(strings -> strings.forEach(result::append));
 
         return result.toString();
-
     }
 }
