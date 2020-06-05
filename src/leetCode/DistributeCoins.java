b1d1d3f3504338658495ff58c37e51c5cef3a5bc
ccota <crcotag@gmail.com>
2020-06-05 09:33:49 -0700

    array approach

--- src/leetCode/DistributeCoins.java
+++ src/leetCode/DistributeCoins.java
@@ -4,20 +4,16 @@ import treesAndGraphs.TreeNode;
 
 public class DistributeCoins {
     public int distributeCoins(TreeNode root) {
-        Result result = new Result();
+        int [] result = new int [2];
         helper(root, result);
-        return result.moves;
+        return result[1];
     }
 
-    int helper(TreeNode node, Result result) {
-        if (node == null) return 0;
+    int helper(TreeNode node, int [] result) {
+        if (node == null) return result[0];
         int leftCount = helper(node.left, result);
         int rightCount = helper(node.right, result);
-        result.moves += Math.abs(leftCount) + Math.abs(rightCount);
+        result[1] += Math.abs(leftCount) + Math.abs(rightCount);
         return node.val + leftCount + rightCount - 1;
     }
-}
-
-class Result {
-    int moves = 0;
 }
