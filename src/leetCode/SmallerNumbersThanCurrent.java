6d9fab5032184cd34eed76a39d46de836a4bbb92
ccota <crcotag@gmail.com>
2020-06-15 22:11:29 -0700

    improve algorithm for smaller numbers than current

--- src/leetCode/SmallerNumbersThanCurrent.java
+++ src/leetCode/SmallerNumbersThanCurrent.java
@@ -1,12 +1,17 @@
 package leetCode;
 
+import java.util.Arrays;
+
 public class SmallerNumbersThanCurrent {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] result = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
+        int[] original = nums.clone();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < result.length; i++) {
             int total = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (i == j || nums[i] < nums[j]) continue;
+            int currentOriginal = original[i];
+            while (nums[total] < currentOriginal) {
                 total++;
             }
             result[i] = total;
