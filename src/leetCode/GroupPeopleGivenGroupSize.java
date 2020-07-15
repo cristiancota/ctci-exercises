bef02251aeba316818fcd5efa90ad9382f9c212a
ccota <crcotag@gmail.com>
2020-07-14 21:53:24 -0700

    refactor on group the people given the group size exercise

--- src/leetCode/GroupPeopleGivenGroupSize.java
+++ src/leetCode/GroupPeopleGivenGroupSize.java
@@ -1,39 +1,39 @@
 package leetCode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GroupPeopleGivenGroupSize {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        Map<Integer, List<Integer>> mapSizeIndex = new HashMap<>();
+        Map<Integer, List<List<Integer>>> mapSizeIndex = new HashMap<>();
         for (int i = 0; i < groupSizes.length; i++) {
-            final int currentGroup = groupSizes[i];
-            if (mapSizeIndex.get(currentGroup) != null) {
-                List<Integer> integers = new ArrayList<>(mapSizeIndex.get(currentGroup));
-                integers.add(i);
-                mapSizeIndex.put(currentGroup, integers);
-            } else {
-                mapSizeIndex.put(currentGroup, Collections.singletonList(i));
-            }
-        }
-
-        List<List<Integer>> ans = new ArrayList<>();
-        mapSizeIndex.forEach((limit, list) -> {
-            if (list.size() > limit) {
-                List<Integer> auxiliaryList = new ArrayList<>();
-                for (Integer integer : list) {
-                    auxiliaryList.add(integer);
-                    if (auxiliaryList.size() == limit) {
-                        ans.add(new ArrayList<>(auxiliaryList));
-                        auxiliaryList.clear();
+            final int groupLimit = groupSizes[i];
+            if (mapSizeIndex.get(groupLimit) != null) {
+                List<List<Integer>> lists = mapSizeIndex.get(groupLimit);
+                boolean addNewList = true;
+                for (List<Integer> list : lists) {
+                    if (list.size() < groupLimit) {
+                        list.add(i);
+                        addNewList = false;
+                        break;
                     }
                 }
-                if (auxiliaryList.size() > 0) {
-                    ans.add(auxiliaryList);
+                if (addNewList) {
+                    ArrayList<Integer> integers = new ArrayList<>();
+                    integers.add(i);
+                    lists.add(integers);
                 }
             } else {
-                ans.add(list);
+                List<List<Integer>> list = new ArrayList<>();
+                list.add(new ArrayList<>());
+                list.get(0).add(i);
+                mapSizeIndex.put(groupLimit, list);
             }
-        });
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        mapSizeIndex.values().forEach(ans::addAll);
         return ans;
     }
 }
