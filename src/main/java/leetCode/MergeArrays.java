1725d769777fc30076be11e5bcf1161f95049649
ccota <crcotag@gmail.com>
2020-07-17 22:05:54 -0700

    merge arrays

---
+++ src/main/java/leetCode/MergeArrays.java
@@ -0,0 +1,19 @@
+package leetCode;
+
+public class MergeArrays {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = nums1.length - 1;
+        m--;
+        n--;
+        while (n >= 0) {
+            if (m >= 0 && nums1[m] >= nums2[n]) {
+                nums1[index] = nums1[m];
+                m--;
+            } else {
+                nums1[index] = nums2[n];
+                n--;
+            }
+            index--;
+        }
+    }
+}
