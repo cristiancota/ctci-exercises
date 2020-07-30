39caf80f4c9ff14bdb0e627e45e4591850d83e23
ccota <crcotag@gmail.com>
2020-07-29 21:55:55 -0700

    129. Sum Root to Leaf Numbers

---
+++ src/main/java/leetCode/SumRootToLeafNumbers.java
@@ -0,0 +1,32 @@
+package leetCode;
+
+import treesAndGraphs.TreeNode;
+
+//      2
+//     / \
+//    3   4
+//   / \
+//  2   1
+
+public class SumRootToLeafNumbers {
+    int sumNumbers(TreeNode tree) {
+        if (tree == null) return 0;
+        return sumNumberHelper(tree, 0);
+    }
+
+    private int sumNumberHelper(TreeNode node, int val) {
+        if (node != null) {
+            if (isLeaf(node)) {
+                return (val * 10) + node.val;
+            } else {
+                val *= 10;
+                return sumNumberHelper(node.left, val + node.val) + sumNumberHelper(node.right, val + node.val);
+            }
+        }
+        return 0;
+    }
+
+    private boolean isLeaf(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+}
