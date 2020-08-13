18f0b60e871c4fc1209c6d0641ea2c39f883f9ec
ccota <crcotag@gmail.com>
2020-08-12 23:29:32 -0700

    1207. Unique Number of Occurrences

---
+++ src/main/java/leetCode/UniqueNumberOfOccurrences.java
@@ -0,0 +1,23 @@
+package leetCode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int number : arr) {
+            int prevValue = counts.getOrDefault(number, 0);
+            counts.put(number, ++prevValue);
+        }
+        Set<Integer> e = new HashSet<>();
+        for (Integer value : counts.values()) {
+            if (!e.add(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
