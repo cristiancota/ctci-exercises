f3659ecf46b18d286d72942eb18ee41623353ca1
ccota <crcotag@gmail.com>
2020-08-17 19:05:37 -0700

    101. Symmetric Tree

---
+++ src/main/java/leetCode/SymmetricTree.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+import treesAndGraphs.TreeNode;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
