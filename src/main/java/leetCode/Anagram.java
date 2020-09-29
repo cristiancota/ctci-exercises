5608be548bd41ffb6b248799e1b1414ab19cfac1
ccota <crcotag@gmail.com>
2020-09-28 22:23:06 -0700

    second solution for valid anagram

--- src/main/java/leetCode/Anagram.java
+++ src/main/java/leetCode/Anagram.java
@@ -1,6 +1,8 @@
 package leetCode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Anagram {
     public boolean isAnagram(String s, String t) {
@@ -15,6 +17,33 @@ public class Anagram {
 
         for (int i = 0; i < a.length; i++) {
             if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isAnagramMap(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.merge(s.charAt(i), 1, Integer::sum);
+            map2.merge(t.charAt(i), 1, Integer::sum);
+        }
+
+        if (map.size() != map2.size()) {
+            return false;
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            final Integer value = map2.get(entry.getKey());
+            if (value == null || !value.equals(entry.getValue())) {
                 return false;
             }
         }
