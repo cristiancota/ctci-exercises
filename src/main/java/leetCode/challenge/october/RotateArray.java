153e25c3a17100240037ab76ec25949c9829bef9
ccota <crcotag@gmail.com>
2020-10-15 15:47:50 -0700

    o(n) solution

--- src/main/java/leetCode/challenge/october/RotateArray.java
+++ src/main/java/leetCode/challenge/october/RotateArray.java
@@ -1,14 +1,22 @@
 package leetCode.challenge.october;
 
+
 public class RotateArray {
     public void rotate(int[] nums, int k) {
-        if (k >= nums.length) {
-            k = k % nums.length;
-        }
-        for (int i = 0; i < k; i++) {
-            int temp = nums[nums.length - 1];
-            System.arraycopy(nums, 0, nums, 1, nums.length - 1);
-            nums[0] = temp;
+        k = k % nums.length;
+
+        int[] array1 = new int[k];
+        int[] array2 = new int[nums.length - k];
+
+        System.arraycopy(nums, nums.length - k, array1, 0, k);
+        System.arraycopy(nums, 0, array2, 0, nums.length - k);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i < k) {
+                nums[i] = array1[i];
+            } else {
+                nums[i] = array2[i - k];
+            }
         }
     }
 }
