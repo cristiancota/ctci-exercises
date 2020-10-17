aa8530e9b005ef57e5c7a9e719082e6669f5b5c9
ccota <crcotag@gmail.com>
2020-10-17 14:48:43 -0700

    two sets solution

--- src/main/java/leetCode/challenge/october/RepeatedDNA.java
+++ src/main/java/leetCode/challenge/october/RepeatedDNA.java
@@ -4,26 +4,15 @@ import java.util.*;
 
 public class RepeatedDNA {
     public List<String> findRepeatedDnaSequences(String s) {
-        if (s.length() == 10) { return Collections.emptyList(); }
-        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
         for (int i = 10; i <= s.length(); i++) {
             String substring = s.substring(i - 10, i);
-            map.putIfAbsent(substring, 0);
-            map.put(substring, map.get(substring) + 1);
-        }
-
-        List<String> ans = new ArrayList<>();
-        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
-            if (stringIntegerEntry.getValue() > 1) {
-                ans.add(stringIntegerEntry.getKey());
+            if (!set.add(substring)) {
+                repeated.add(substring);
             }
         }
 
-        return ans;
-    }
-
-    public static void main(String[] args) {
-        RepeatedDNA repeatedDNA = new RepeatedDNA();
-        repeatedDNA.findRepeatedDnaSequences("AAAAAAAAAAA");
+        return new ArrayList<>(repeated);
     }
 }
