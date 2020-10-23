f104461835467f04c96c037a4b014579d2457df8
ccota <crcotag@gmail.com>
2020-10-23 15:20:30 -0700

    132 Pattern

---
+++ src/main/java/leetCode/challenge/october/Pattern132.java
@@ -0,0 +1,16 @@
+package leetCode.challenge.october;
+
+public class Pattern132 {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        int leftMin = nums[0];
+        for (int j = 1; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (nums[k] > leftMin && nums[j] > nums[k])
+                    return true;
+            }
+            leftMin = Math.min(nums[j], leftMin);
+        }
+        return false;
+    }
+}
