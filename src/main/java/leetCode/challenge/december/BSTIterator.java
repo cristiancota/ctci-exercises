73074a6f622eb74b4d6b6ef2becdec3f850b0e32
ccota <crcotag@gmail.com>
2020-12-09 21:55:56 -0700

    Binary Search Tree Iterator

---
+++ src/main/java/leetCode/challenge/december/BSTIterator.java
@@ -0,0 +1,37 @@
+package leetCode.challenge.december;
+
+import treesAndGraphs.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BSTIterator {
+    List<Integer> list = new ArrayList<>();
+    int index = -1;
+
+    public BSTIterator(TreeNode root) {
+        if (root != null) {
+            helper(root);
+        }
+    }
+
+    private void helper(TreeNode node) {
+        if (node != null) {
+            helper(node.left);
+            list.add(node.val);
+            helper(node.right);
+        }
+    }
+
+    public int next() {
+        if (hasNext()) {
+            index++;
+            return list.get(index);
+        }
+        return -1;
+    }
+
+    public boolean hasNext() {
+        return index < list.size();
+    }
+}
