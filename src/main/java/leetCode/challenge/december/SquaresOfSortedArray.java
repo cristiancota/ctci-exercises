29ae2d363bbb913ee94a04572e88c84ceec8cf56
ccota <crcotag@gmail.com>
2020-12-15 21:22:26 -0700

    Squares Of Sorted Array

---
+++ src/main/java/leetCode/challenge/december/SquaresOfSortedArray.java
@@ -0,0 +1,13 @@
+package leetCode.challenge.december;
+
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] *= nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
