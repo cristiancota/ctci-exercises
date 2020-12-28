86e18ac806be1e9a7a17cc2553669c71007d28b7
ccota <crcotag@gmail.com>
2020-12-28 16:23:30 -0700

    refactor on recursive call, get rid of index on BSTIterator

--- src/main/java/leetCode/challenge/december/BSTIterator.java
+++ src/main/java/leetCode/challenge/december/BSTIterator.java
@@ -2,35 +2,34 @@ package leetCode.challenge.december;
 
 import treesAndGraphs.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class BSTIterator {
-    List<Integer> list = new ArrayList<>();
-    int index = -1;
+    Queue<Integer> queue = new LinkedList<>();
 
     public BSTIterator(TreeNode root) {
         if (root != null) {
-            helper(root);
+            fillQueue(root);
         }
     }
 
-    private void helper(TreeNode node) {
+    private void fillQueue(TreeNode node) {
         if (node != null) {
-            helper(node.left);
-            list.add(node.val);
-            helper(node.right);
+            fillQueue(node.left);
+            queue.add(node.val);
+            fillQueue(node.right);
         }
     }
 
     public int next() {
         if (hasNext()) {
-            return list.get(++index);
+            return queue.remove();
         }
         return -1;
     }
 
     public boolean hasNext() {
-        return index < list.size() - 1;
+        return !queue.isEmpty();
     }
 }
--- src/main/java/leetCode/challenge/december/MaximumDepthOfBinaryTree.java
+++ src/main/java/leetCode/challenge/december/MaximumDepthOfBinaryTree.java
@@ -4,18 +4,16 @@ import treesAndGraphs.TreeNode;
 
 class MaximumDepthOfBinaryTree {
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return maxDepth(root, 1);
+        return maxDepth(root, 0);
     }
 
     private int maxDepth(TreeNode node, int max) {
         if (node != null) {
+            max++;
             if (node.left == null && node.right == null) {
                 return max;
             } else {
-                return Math.max(maxDepth(node.left, max + 1), maxDepth(node.right, max + 1));
+                return Math.max(maxDepth(node.left, max), maxDepth(node.right, max));
             }
         }
 
