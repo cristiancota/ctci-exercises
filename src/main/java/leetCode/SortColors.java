246ab82a6aa9844fce3c443bdac9436cf6efd771
ccota <crcotag@gmail.com>
2021-02-09 23:17:30 -0700

    Sort colors better solution

--- src/main/java/leetCode/SortColors.java
+++ src/main/java/leetCode/SortColors.java
@@ -1,9 +1,30 @@
 package leetCode;
 
-import java.util.Arrays;
-
 public class SortColors {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        // 1ms
+        //Arrays.sort(nums);
+
+        // 0ms
+        int pivot = 1;
+        int left = 0;
+        int index = 0;
+        int right = nums.length - 1;
+
+        while (index <= right) {
+            if (nums[index] < pivot) {
+                swap(nums, index++, left++);
+            } else if (nums[index] > pivot) {
+                swap(nums, index, right--);
+            } else {
+                index++;
+            }
+        }
+    }
+
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
