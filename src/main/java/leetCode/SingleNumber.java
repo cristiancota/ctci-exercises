aafde5efa7310a8faa9cd4c877d71235e38aea60
Cristian Cota <crcotag@gmail.com>
2021-04-05 18:49:09 -0700

    137. Single Number II

--- src/main/java/leetCode/SingleNumber.java
+++ src/main/java/leetCode/SingleNumber.java
@@ -13,4 +13,21 @@ public class SingleNumber {
         }
         return ans;
     }
+    
+    public int singleNumberII(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.putIfAbsent(num, 0);
+            Integer integer = map.get(num);
+            map.put(num, integer + 1);
+        }
+
+        for (int num : nums) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
 }
