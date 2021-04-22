084956247e982d2da3239045997a02e1e0ba5114
ccota <crcotag@gmail.com>
2021-04-21 22:00:29 -0700

    Second approach to validate BST

--- src/main/java/leetCode/challenge/december/ValidBST.java
+++ src/main/java/leetCode/challenge/december/ValidBST.java
@@ -6,6 +6,7 @@ public class ValidBST {
     public boolean isValidBST(TreeNode node) {
         return helper(node, null, null);
     }
+
     private boolean helper(TreeNode node, Integer lower, Integer upper) {
         if (node == null) return true;
 
@@ -15,5 +16,29 @@ public class ValidBST {
         if (upper != null && val >= upper) return false;
         if (!helper(node.left, lower, val)) return false;
         return helper(node.right, val, upper);
+    }
+
+    TreeNode prev;
+
+    public boolean isValidBST2(TreeNode node) {
+        if (node == null) {
+            return true;
+        }
+
+        // inorder => left -> root -> right
+        if (!isValidBST2(node.left)) {
+            return false;
+        }
+
+        if (prev != null) {
+            // if prev is greater or equal than current is invalid BST
+            if (prev.val >= node.val) {
+                return false;
+            }
+        }
+
+        prev = node;
+
+        return isValidBST2(node.right);
     }
 }
